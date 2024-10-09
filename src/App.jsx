0451645df635a5a7caf5095f10cb4e1ca71c9bce
princejacob528 <princejacob528@gmail.com>
2024-10-09 11:52:18 +0530

    updated for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Banner from "./components/Banner/banner";
