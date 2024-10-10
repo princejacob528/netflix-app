407794acd6403be72cb0e6e2fdfb5a252064a2f8
Prince Jacob <41814394+princejacob528@users.noreply.github.com>
2024-10-10 08:30:11 +0530

    Update constants.jsx

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
-export const API_KEY = "25d7de54ec7b104859616db339c19f23";
+export const API_KEY = "yourApiKey";
 export const imageUrl = 'https://image.tmdb.org/t/p/original';
