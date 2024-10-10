0040b0a40f9d4a6f67184dffbb47ddf2cfbf4b6d
princejacob528 <princejacob528@gmail.com>
2024-10-10 09:22:21 +0530

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import RowPost from "./components/RowPost/RowPost";
 import { baseUrl, API_KEY } from "./constants/constants";
 
 function App() {
-  console.log(process.env.TDMB_API_KEY)
+  console.log(process.env.REACT_APP_TMDB_API_KEY)
   const items = [
     {
       title: "NETFLIX ORGINALS",
